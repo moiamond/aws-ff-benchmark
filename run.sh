919cea527c8171cc6dc645ba4439c844eb734810
moiamond <moiamond@gmail.com>
2016-11-03 10:57:01 +0800

    Handle Key Pair

--- run.sh
+++ run.sh
@@ -3,6 +3,11 @@
 s3_url=$1
 ec2_type=t2.micro
 
+# create key pair
+aws ec2 create-key-pair --key-name $(uname -n) --query "KeyMaterial" --output text > $(uname -n).pem
+chmod 400 $(uname -n).pem
+echo Create Key Pair ... $(uname -n).pem
+
 # launch ec2 instance
 instance_id=$(aws ec2 run-instances --image-id ami-0c11b26d --security-group-ids sg-98b474ff --count 1 --instance-type $ec2_type --key-name $(uname -n) --output text --query 'Instances[*].InstanceId')
 echo instance_id=$instance_id
@@ -21,9 +26,9 @@ aws ec2 get-console-output --instance-id $instance_id --output text | perl -ne "
 sleep 60
 
 # ssh
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $(uname -n).pem ec2-user@$ip_address  "sudo curl -sSL https://get.docker.com/ | sh; sudo usermod -aG docker ec2-user; sudo /etc/init.d/docker restart" | tee output1.txt
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $(uname -n).pem ec2-user@$ip_address  "sudo curl -sSL https://get.docker.com/ | sh; sudo usermod -aG docker ec2-user; sudo /etc/init.d/docker restart"
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $(uname -n).pem ec2-user@$ip_address  "docker pull jrottenberg/ffmpeg" | tee output2.txt
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $(uname -n).pem ec2-user@$ip_address  "docker pull jrottenberg/ffmpeg"
 
 # https://s3-ap-northeast-1.amazonaws.com/bssff-test/ec2_1min.mxf
 echo Downloading ... $1
@@ -37,3 +42,5 @@ aws ec2 terminate-instances --instance-ids "$instance_id" --output text --query 
 
 # delete key pair
 aws ec2 delete-key-pair --key-name $(uname -n)
+rm -f $(uname -n).pem
+echo Delete Key Pair ... $(uname -n).pem
